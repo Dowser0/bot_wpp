27730dd86f39c53cea3e6403a6a5df5f8a5f59c8
Dowser <gabrielpestana37@gmail.com>
2020-12-29 22:06:11 -0300

    first commit

---
+++ _func_.py
@@ -0,0 +1,194 @@
+import requests
+import dicionario_de_moedas
+import matplotlib.pyplot as plt
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.keys import Keys
+from pytube import YouTube,Playlist
+import os
+from googleapi import google
+from bs4 import BeautifulSoup
+
+
+imagem = "C:/Users/gabri/Documents/bot_wpp/figura.png"
+
+dicionario_moedas = dicionario_de_moedas.dicionario_moedas()
+
+motorista = webdriver.Chrome(ChromeDriverManager().install())
+
+motorista.get('https://web.whatsapp.com')
+
+def buscar_contato(contato):
+
+    campo_pesquisa = motorista.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+
+    campo_pesquisa[0].click()
+
+    campo_pesquisa[0].send_keys(contato)
+
+    campo_pesquisa[0].send_keys(Keys.ENTER)
+
+def ler_ultima_mensagem():
+
+    caixa_de_mensagem =  motorista.find_elements_by_xpath('//span[contains(@class,"selectable-text invisible-space copyable-text")]')
+
+    ultima_mensagem = caixa_de_mensagem[-1].text
+
+    return ultima_mensagem
+
+def enviar_mensagem(mensagem):
+
+    mensagem = str(mensagem)
+
+    campo_mensagem = motorista.find_elements_by_xpath('//div[contains(@class,"copyable-text selectable-text")]')
+
+    campo_mensagem[1].click()
+
+    campo_mensagem[1].send_keys(mensagem)
+
+    campo_mensagem[1].send_keys(Keys.ENTER)
+
+def enviar_midia(midia):
+
+    motorista.find_element_by_xpath ('//*[@id="main"]/footer/div[1]/div[1]/div[2]/div/div/span').click()
+
+    time.sleep(0.5)
+
+    motorista.find_element_by_xpath("//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']").send_keys(midia)
+
+    time.sleep(3)
+
+    campo_de_envio = motorista.find_elements_by_xpath('//div[contains(@class,"q2PP6 _3Git-")]')
+
+    campo_de_envio[0].click()
+
+def _request_atual(nome_da_moeda):
+    #Request money
+    r_get = requests.get('https://economia.awesomeapi.com.br/json/all/'+nome_da_moeda)
+    if r_get.status_code == 200:
+        return r_get.json()
+    else:
+        r_get.status_code
+
+#def _request_dias(nome_da_moeda,dias):
+#    #Requset money
+#    r_get = requests.get('https://economia.awesomeapi.com.br/json/daily/'+nome_da_moeda+'/'+dias)
+#    if r_get.status_code == 200:
+#        return r_get.json()
+#    else:
+#        r_get.status_code
+
+def request_valor_atual(nome_da_moeda):
+    nome_da_moeda = nome_da_moeda.upper()
+    if nome_da_moeda in dicionario_moedas:
+        r_1 = _request_atual(dicionario_moedas[nome_da_moeda][0])
+        parametro_da_moeda = dicionario_moedas[nome_da_moeda][1]
+        valor = float(r_1[parametro_da_moeda]['bid'])
+        return [valor,nome_da_moeda]
+    else:
+        None
+
+#def historico_de_valor_de_uma_moeda(nome_do_dinheiro,dias):
+#    r_1 = _request_dias(nome_do_dinheiro,dias)
+#    lista_de_valores = []
+#    lista_de_dias = []
+#    contador = 1
+
+#    for i in range(0,int(dias)):
+#        resposta_bid = r_1[i]['bid']
+        #resposta_date = r[i]['timestamp']
+        #resposta_date_convert = datetime.datetime.fromtimestamp(int(resposta_date))
+#        lista_de_valores.append(float(resposta_bid))
+#        lista_de_dias.append(contador)
+#        contador +=1
+
+#    return [lista_de_dias,lista_de_valores]
+
+
+#def separa_moeda_e_dias(nome_da_moeda_com_dias):
+#    lista_moeda_dias = nome_da_moeda_com_dias.split('-')
+#    if isinstance(lista_moeda_dias,list):
+#        return lista_moeda_dias
+#    else:
+#        return None
+
+#def gera_grafico(nome_da_moeda,lista_de_dias,lista_de_valores):
+#    plt.clf()
+#    plt.title(nome_da_moeda)
+#    plt.xlabel('Dias atrás')
+#    plt.ylabel('Preço de venda') 
+
+#    plt.scatter(lista_de_dias,lista_de_valores)
+#    plt.plot(lista_de_dias,lista_de_valores)
+    #plt.legend()
+
+#    plt.savefig('figura.png')
+#    return 'figura.png'
+
+def download_youtube(url):
+    yt = YouTube(url)
+    audio = yt.streams.filter(only_audio=True)[0]
+    music = audio.download()
+    os.rename(music,music[:-4]+'.mp3')
+    return music[:-4]+'.mp3'
+
+#def download_playlist_envia(url_):                                                  
+#    playlist = Playlist(url_)
+#    print(playlist)
+#    for url in playlist:
+#        yt = YouTube(url)
+#        audio = yt.streams.filter(only_audio=True)[0]
+#        music = audio.download()
+#        os.rename(music,music[:-4]+'.mp3')
+#        enviar_midia(music[:-4]+'.mp3')
+#        deleta_arquivo(music[:-4]+'.mp3')
+#        time.sleep(1)
+    
+def deleta_arquivo(arquivo):
+    os.remove(arquivo)
+
+def request_runas(campeao):
+    html = requests.get("https://rankedboost.com/league-of-legends/build/"+campeao+"/").content
+    soup = BeautifulSoup(html, 'html.parser')
+    elemento1 = soup.find_all("div", class_="rb-build-rune-text")
+    lista_de_runas = []
+    for z in range(len(elemento1)):
+        lista_de_runas.append(elemento1[z].string)
+
+    return lista_de_runas
+
+def request_habilidades(campeao):
+    html = requests.get("https://rankedboost.com/league-of-legends/build/"+campeao+"/").content
+    soup = BeautifulSoup(html, 'html.parser')
+    elemento1 = soup.find_all("div", class_="rb-build-ability-key")
+    ordem_das_habilidades = []
+    for z in range(len(elemento1)):
+        ordem_das_habilidades.append(elemento1[z].string)
+    return ordem_das_habilidades
+
+def request_build(campeao):
+    html = requests.get("https://rankedboost.com/league-of-legends/build/"+campeao+"/").content
+    soup = BeautifulSoup(html, 'html.parser')
+    elemento1 = soup.find_all("span", class_="rb-item-img-text obt-css")
+    build = []
+    for z in range(len(elemento1)):
+        build.append(elemento1[z].string)
+    return build
+
+def request_counter(campeao):
+    html = requests.get("https://rankedboost.com/league-of-legends/build/"+campeao+"/").content
+    soup = BeautifulSoup(html, 'html.parser')
+    elemento1 = soup.find_all("span", class_="counters-sidebar-champ-name")
+    counters = []
+    for z in range(len(elemento1)):
+        counters.append(elemento1[z].string)
+    return counters
+
+
+
+def request_pesquisa(pesquisa):
+    resultados = google.search(pesquisa+" youtube")
+    pesquisa = [resultados[0].link,resultados[0].name]
+    if pesquisa[0].startswith('https://www.youtube.com/'):
+        return pesquisa
---
+++ main.py
@@ -0,0 +1,116 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.keys import Keys
+import _func_
+import dicionario_de_moedas
+import random
+
+
+contato = ['weasel gaming club']
+mensagem = 'top'
+imagem = "C:/Users/gabri/Documents/bot_wpp/figura.png"
+dicionario_moedas = dicionario_de_moedas.dicionario_moedas()
+run = True
+
+
+time.sleep(10)
+_func_.buscar_contato(contato)
+time.sleep(0.5)
+
+while run:
+    try:
+
+        ultima_mensagem = str(_func_.ler_ultima_mensagem())
+
+        if ultima_mensagem.lower().startswith('!money'):
+            nome_da_moeda = ultima_mensagem[6:]
+            valor_atual = _func_.request_valor_atual(nome_da_moeda)
+            valor_atual[0] = str(round(valor_atual[0], 2))
+            mensagem = valor_atual[1]+':R$'+valor_atual[0]
+            _func_.enviar_mensagem(mensagem)
+
+#        if ultima_mensagem.lower().startswith('!mhistoric'):
+#            nome_da_moeda = ultima_mensagem[10:]
+#            moeda_com_dias = _func_.separa_moeda_e_dias(nome_da_moeda)
+#            nome_da_moeda = moeda_com_dias[0].upper()
+#            parametro_da_moeda = dicionario_moedas[nome_da_moeda][0]
+#            dias = moeda_com_dias[1]
+#            lista_historico_de_valores_e_dias = _func_.historico_de_valor_de_uma_moeda(parametro_da_moeda,dias)
+#            lista_de_dias = lista_historico_de_valores_e_dias[0]
+#            lista_de_valores = lista_historico_de_valores_e_dias[1]
+#            _func_.gera_grafico(nome_da_moeda,lista_de_dias,lista_de_valores)
+#            mensagem =  'seu gráfico :)'
+#            _func_.enviar_mensagem(mensagem)
+#            _func_.enviar_midia(imagem)
+
+        if ultima_mensagem.lower().startswith('!md'):
+            pesquisa = ultima_mensagem[3:]
+            if pesquisa.lower().startswith('https://www.youtube.com/'):
+                _func_.enviar_mensagem('Fazendo download de '+pesquisa)
+                nome_do_arquivo = _func_.download_youtube(pesquisa)
+                _func_.enviar_midia(nome_do_arquivo)
+                _func_.deleta_arquivo(nome_do_arquivo)
+            else : 
+                resultados = _func_.request_pesquisa(pesquisa)
+                _func_.enviar_mensagem('Fazendo download de '+resultados[1][0:-33])
+                nome_do_arquivo = _func_.download_youtube(resultados[0])
+                _func_.enviar_midia(nome_do_arquivo)
+                _func_.deleta_arquivo(nome_do_arquivo)
+
+        if ultima_mensagem.lower().startswith('!roll'):
+            num_sorteado = random.randint(1,6)
+            mensagem = num_sorteado
+            _func_.enviar_mensagem(mensagem)
+
+        if ultima_mensagem.lower().startswith('!rune'):
+            campeao = ultima_mensagem[5:]
+            runas = _func_.request_runas(campeao)
+            mensagem = '*'+runas[0]+'*\n'+runas[1]+'\n'+runas[2]+'\n'+runas[3]+'\n'+runas[4]+'\n*'+runas[5]+'*\n'+runas[6]+'\n'+runas[7]+'\n*'+runas[8]+'*\n*'+runas[9]+'*\n*'+runas[10]+'*\n'
+            _func_.enviar_mensagem(mensagem)
+
+        if ultima_mensagem.lower().startswith('!skill'):
+            campeao = ultima_mensagem[6:]
+            habilidade = _func_.request_habilidades(campeao)
+            mensagem = '*'+habilidade[0]+'* > *'+habilidade[1]+'* > *'+habilidade[2]+'*'
+            _func_.enviar_mensagem(mensagem)
+        
+        if ultima_mensagem.lower().startswith('!build'):
+            campeao = ultima_mensagem[6:]
+            build = _func_.request_build(campeao)
+            mensagem = build[0]+'\n'+build[1]+'\n'+build[2]+'\n'+build[3]+'\n'+build[4]+'\n'+build[5]
+            _func_.enviar_mensagem(mensagem)
+
+        if ultima_mensagem.lower().startswith('!champ'):
+            campeao = ultima_mensagem[6:]
+            mensagem = '*'+runas[0]+'*\n'+runas[1]+'\n'+runas[2]+'\n'+runas[3]+'\n'+runas[4]+'\n*'+runas[5]+'*\n'+runas[6]+'\n'+runas[7]+'\n*'+runas[8]+'*\n*'+runas[9]+'*\n*'+runas[10]+'*\n'
+            _func_.enviar_mensagem(mensagem)
+            habilidade = _func_.request_habilidades(campeao)
+            mensagem = '*'+habilidade[0]+'* > *'+habilidade[1]+'* > *'+habilidade[2]+'*'
+            _func_.enviar_mensagem(mensagem)
+            build = _func_.request_build(campeao)
+            mensagem = build[0]+'\n'+build[1]+'\n'+build[2]+'\n'+build[3]+'\n'+build[4]+'\n'+build[5]
+            _func_.enviar_mensagem(mensagem)
+
+        if ultima_mensagem.lower().startswith('!counter'):
+            campeao = ultima_mensagem[8:]
+            counters = _func_.request_counter(campeao)
+            mensagem = '*Ban suggestions*\n'+counters[-1]+'\n'+counters[-2]+'\n'+counters[-3]+'\n*Counter picks*\n'+counters[-4]+'\n'+counters[-5]+'\n'+counters[-6]+'\n'
+            _func_.enviar_mensagem(mensagem)
+
+        #if message.content.startswith('!'):
+        
+
+        if ultima_mensagem.lower().startswith('!help'):
+            mensagem = '!roll\n!md (link or name)\n!rune (name champ)\n!skill (name champ)\n!build (name champ)\n!champ (name champ)\n!counter (name champ)'
+            _func_.enviar_mensagem(mensagem)
+
+    except:
+        pass
+
+
+
+
+
+
+
